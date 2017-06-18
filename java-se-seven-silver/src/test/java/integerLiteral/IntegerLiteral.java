a41caa3af49aed90dd4c1baf484c66a64bd71c26
taka <521taka.m@gmail.com>
2017-06-18 22:03:39 +0900

    リテラル表記のメソッドを追加

--- java-se-seven-silver/src/test/java/integerLiteral/IntegerLiteral.java
+++ java-se-seven-silver/src/test/java/integerLiteral/IntegerLiteral.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 
 public class IntegerLiteral {
 
+    /**
+     * 整数リテラルのn進数別の表記方法について。
+     */
     @Test
-    public void test_整数リテラルの表記(){
+    public void test_整数リテラルの進数別表記(){
 
         // 基本的な整数リテラル（10進数）
         int a = 10;
@@ -25,5 +28,42 @@ public class IntegerLiteral {
         assertEquals(expected, b);
         assertEquals(expected, c);
         assertEquals(expected, d);
+    }
+
+    /**
+     * 整数リテラルの「_(アンダースコア)」表記について。<br/>
+     *
+     * ・リテラルの先頭と末尾には記述できない。<br/>
+     * ・記号の前後には記述できない。<br/>
+     *
+     * ここで言う「記号」とは「.(ドット)」や、floatを表す「F」、longを表す「L」、
+     * 2進数を表す「0b」、16進数を表す「0x」などが含まれる。
+     */
+    @Test
+    public void test_整数リテラルのアンダースコア表記(){
+
+        // 基本的な記述
+        int a = 10_000;
+
+        // アンスコの個数は自由
+        int b = 10__________000;
+
+        // 2進数にも使用可能
+        int c = 0b1_0;
+
+        // 16進数にも使用可能
+        int d = 0xF_0;
+
+        // リテラルの先頭は禁止
+        // int e = _123_456;
+
+        // リテラルの末尾も禁止
+        // int f = 123_456_;
+
+        // 記号の直前は禁止。
+        // int g = 1_.001;
+
+        // 記号の直後も禁止。
+        // int h = 0x_10;
     }
 }
