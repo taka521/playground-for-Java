688160e795930396bbacbc7183cf9b633d00c524
taka <521taka.m@gmail.com>
2017-07-01 17:38:23 +0900

    POSTリクエストでJSON Streamを受け取るメソッドを追加

--- webflax-demo/src/main/java/com/example/controller/HelloController.java
+++ webflax-demo/src/main/java/com/example/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -25,7 +28,7 @@ public class HelloController {
     }
 
     @GetMapping("/streamNoLimit")
-    Flux<Map<String, Integer>> streamNoLimit(){
+    Flux<Map<String, Integer>> streamNoLimit() {
         Stream<Integer> stream = Stream.iterate(0, i -> i + 1);
         return Flux.fromStream(stream).zipWith(Flux.interval(Duration.ofSeconds(1)))
                 .map(tuple -> Collections.singletonMap("value", tuple.getT1()));
@@ -34,5 +37,29 @@ public class HelloController {
         // で、それをFlux<Stream>とzipする。
         // 上記の例だと、毎秒１件ずつデータが返ってくる
 
+    }
+
+    @PostMapping("/echo")
+    Mono<String> echo(@RequestBody Mono<String> body) {
+        return body.map(String::toUpperCase);
+
+        /*
+            @RequestBodyで、リクエストボディを受け取ることができる。
+            で、それをMonoでラップすることによって非同期でチェインできる。
+            (Monoでラップしなかった場合には、ノンブロッキングで同期な処理になる）
+            Monoは0...1要素のPublisher。
+         */
+    }
+
+    @PostMapping("/stream")
+    Flux<Map<String, Integer>> stream(@RequestBody Flux<Map<String, Integer>> body){
+        return body.map(m -> Collections.singletonMap("value", m.get("value") * 2));
+
+        /*
+            リクエストボディに複数の値が来る場合、Fluxでラップする。
+
+            curl -v localhost:8080/stream -d '{"value":1}{"value":2}{"value":3}'
+            -H 'Content-Type: application/stream+json'  -H 'Accept: text/event-stream'
+         */
     }
 }
