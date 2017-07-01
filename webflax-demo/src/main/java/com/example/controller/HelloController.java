02f8c38cfefdfde4428c8dfc6475f1ef1d393d0b
taka <521taka.m@gmail.com>
2017-07-01 16:56:08 +0900

    SpringWebFlaxでHello Worldまで

---
+++ webflax-demo/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    Flux<String> hello(){
+        return Flux.just("Hello", "World");
+    }
+}
