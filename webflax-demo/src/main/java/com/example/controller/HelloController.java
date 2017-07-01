d7b6a08f713e165949ccd83e5c36f33124f91def
taka <521taka.m@gmail.com>
2017-07-01 17:20:18 +0900

    毎秒１件ずつデータが返るように変更

--- webflax-demo/src/main/java/com/example/controller/HelloController.java
+++ webflax-demo/src/main/java/com/example/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -26,6 +27,12 @@ public class HelloController {
     @GetMapping("/streamNoLimit")
     Flux<Map<String, Integer>> streamNoLimit(){
         Stream<Integer> stream = Stream.iterate(0, i -> i + 1);
-        return Flux.fromStream(stream.map(i -> Collections.singletonMap("value", i)));
+        return Flux.fromStream(stream).zipWith(Flux.interval(Duration.ofSeconds(1)))
+                .map(tuple -> Collections.singletonMap("value", tuple.getT1()));
+
+        // Flux.interval(Duration)で指定した時間毎に処理が実行される。
+        // で、それをFlux<Stream>とzipする。
+        // 上記の例だと、毎秒１件ずつデータが返ってくる
+
     }
 }
