1401393a1a35bd2934dbdb17c6754948f5e23170
taka <521taka.m@gmail.com>
2017-07-06 23:25:46 +0900

    Hello Worldのコードを作成

--- rxjava-demo/src/main/java/Main.java
+++ rxjava-demo/src/main/java/Main.java
@@ -1,7 +1,47 @@
+import rx.Observable;
+import rx.Observer;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // "Hello" と "World" を通知するObservable
+        Observable<String> observableGreeting = Observable.create(subscriber -> {
+
+            // 購読解除されている場合は、処理を終了する。
+            if (subscriber.isUnsubscribed()) return;
+
+            subscriber.onNext("Hello"); // 1回目の通知
+            subscriber.onNext("World"); // 2回目の通知
+
+            // 購読解除されてない場合は、完了を通知する。
+            if (!subscriber.isUnsubscribed()) subscriber.onCompleted();
+
+        });
+
+        // Observableを購読し、処理を行う。
+        observableGreeting.subscribe(new Observer<String>() {
+
+            // 完了通知時の処理
+            @Override
+            public void onCompleted() {
+                String threadName = Thread.currentThread().getName();
+                System.out.printf("[%s]完了しました%n", threadName);
+            }
+
+            // エラー通知時の処理
+            @Override
+            public void onError(final Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            // データ通知時の処理
+            @Override
+            public void onNext(final String item) {
+                String threadName = Thread.currentThread().getName();
+                System.out.printf("[%s]%s%n", threadName, item);
+            }
+        });
 
     }
 }
