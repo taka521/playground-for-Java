138cbb133683e41573e6144c1a3445e829125fdb
taka <521taka.m@gmail.com>
2017-07-08 13:42:03 +0900

    observeOnメソッドで、通知待ちのデータをバッファ数を明示的に設定。また、Flowable側で生成したデータのうち、通知されなかったものは破棄するように変更

--- rxjava-demo/src/main/java/rxjava/ver_2/Main.java
+++ rxjava-demo/src/main/java/rxjava/ver_2/Main.java
@@ -1,79 +1,49 @@
 package rxjava.ver_2;
 
-import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.ResourceSubscriber;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
+import io.reactivex.subscribers.DisposableSubscriber;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        // あいさつの文字列を通知する、Flowable（Publisher）の生成
-        Flowable<String> flowable = Flowable.create(flowableEmitter -> {
+        /**
+         * 100ミリ秒ごとに0から始まるデータを通知するFlowable。<br/>
+         * なお、通知できないデータは破棄するようにする。（BackpressureMode.DROPを指定した場合と同じ）
+         */
+        Flowable<Long> flowable = Flowable.interval(100L, TimeUnit.MILLISECONDS).onBackpressureDrop();
 
-            String[] datas = {"Hello", "World"};
-            for (String item : datas) {
-                // 購読がキャンセルされていなければデータを通知
-                if (flowableEmitter.isCancelled()) return;
-                flowableEmitter.onNext(item);
-            }
-
-            flowableEmitter.onComplete(); // 完了したことを通知
-        }, BackpressureStrategy.BUFFER); // 超過したデータはバッファする。
-
-
-        // Subscriberの処理を別スレッドで実行させる。
-        // ResourceSubscriberは、内部でonSubscribeメソッドでLong.MAX_VALUEをリクエストするようになっている抽象クラス。
-        flowable.observeOn(Schedulers.computation()).subscribe(new ResourceSubscriber<String>() {
-
-            /** 開始時間を記録する。 */
-            private long startTime;
+        // 非同期でデータを受け取るようにし、バッファサイズは 1 とする。
+        // バッファサイズは Subscription#request に渡すリクエストの数と意味合いが一緒。
+        flowable.observeOn(Schedulers.computation(), false, 2).subscribe(new DisposableSubscriber<Long>() {
 
             @Override
-            protected void onStart() {
-                startTime = System.currentTimeMillis();
-                this.request(Long.MAX_VALUE);
-            }
-
-            @Override
-            public void onNext(String s) {
-
-                // 購読開始から500ミリを超過した場合には、購読を解除する。
-                long progressTime = System.currentTimeMillis() - startTime;
-                if (progressTime > 500L) {
-                    this.dispose();
-                    System.out.println("購読を解除しました");
-                    return;
-                }
-
-                // 購読を解除させるためにスリープを挟む。
+            public void onNext(Long aLong) {
+                // データの生産 > データの消費 となるように、300ミリスリープさせる。
                 try {
-                    Thread.sleep(1000L);
+                    Thread.sleep(300L);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                    System.exit(1);
                 }
 
-                System.out.printf("[%s]%s%n", Thread.currentThread().getName(), s); // 受け取ったデータを出力
+                System.out.printf("[%s]%d%n", Thread.currentThread().getName(), aLong);
             }
 
             @Override
             public void onError(Throwable t) {
-                t.printStackTrace(); // エラー通知時の処理
+                t.printStackTrace();
             }
 
             @Override
             public void onComplete() {
-                System.out.printf("[%s]完了しました！", Thread.currentThread().getName()); // 完了通知時の処理
+                System.out.printf("[%s]完了しました！%n", Thread.currentThread().getName());
             }
         });
 
-        try {
-            Thread.sleep(1500L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(2000L);
 
     }
 }
