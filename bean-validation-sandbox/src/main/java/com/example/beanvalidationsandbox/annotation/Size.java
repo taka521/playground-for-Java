be392d5a78987a8be442f07e931e803cc5eb55d9
taka <521taka.m@gmail.com>
2017-08-27 16:52:04 +0900

    バリデートアノテーションに@Retentionと@Documentedを注釈

--- bean-validation-sandbox/src/main/java/com/example/beanvalidationsandbox/annotation/Size.java
+++ bean-validation-sandbox/src/main/java/com/example/beanvalidationsandbox/annotation/Size.java
@@ -9,16 +9,16 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
 @Target({FIELD, METHOD, PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RUNTIME)
 @Constraint(validatedBy = {SizeValidatorForString.class, SizeValidatorForInteger.class, SizeValidatorForCollection.class})
 public @interface Size {
 
@@ -33,6 +33,8 @@ public @interface Size {
     int max() default Integer.MAX_VALUE;
 
     @Target({METHOD, FIELD, PARAMETER})
+    @Retention(RUNTIME)
+    @Documented
     @interface List {
         Size[] value();
     }
